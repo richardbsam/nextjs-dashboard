23712229f9636f04dde2335f3a22f1a189453d33
richardbsam <richardbonifacesam@gmail.com>
2025-05-28 16:07:55 +0100

    Downloaeded

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -160,12 +160,14 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+  console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
   }
 }
+
 
 export async function fetchCustomers() {
   try {
